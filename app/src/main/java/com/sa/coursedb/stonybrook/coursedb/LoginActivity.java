5f6b5f571b66a0a42b6d5ec1368957dd05783e4f
Sheryar Shah <sheryar.shah53@yahoo.com>
2016-04-14 02:14:09 -0400

    Added Login and Course Activity-ADDED LAYOUTS

--- app/src/main/java/com/sa/coursedb/stonybrook/coursedb/LoginActivity.java
+++ app/src/main/java/com/sa/coursedb/stonybrook/coursedb/LoginActivity.java
@@ -1,29 +1,88 @@
 package com.sa.coursedb.stonybrook.coursedb;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import java.io.DataOutputStream;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
+
+    KeyStore keyStore;
+
+    private Button login;
+    private EditText user, pass;
+    private CheckBox loginCheckBox;
+    private DataOutputStream cgiInput;
+    private String wrongCredentials = "";
+
+    protected ProgressDialog progressDialog;
+    protected ArrayList<String> courses = new ArrayList<String>();
+    protected boolean serverError = false;
+
+    private SharedPreferences loginPreferences;
+    private SharedPreferences.Editor loginPrefsEditor;
+
+    private SharedPreferences userNamePrefs;
+    private SharedPreferences.Editor userNameEdit;
+
+    private SharedPreferences studentNamePrefs;
+    private SharedPreferences.Editor studentNameEdit;
+
+    public String studentName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+  /*      login = (Button) findViewById(R.id.login);
+        user = (EditText) findViewById(R.id.netid);
+        pass = (EditText) findViewById(R.id.password);
+        loginCheckBox = (CheckBox)findViewById(R.id.checkBox);
+
+        loginPreferences = getSharedPreferences("loginPrefs", MODE_PRIVATE);
+        loginPrefsEditor = loginPreferences.edit();
+
+        coursePrefs = getSharedPreferences("Course", Context.MODE_PRIVATE);
+        courseEdit = coursePrefs.edit();
+
+        studentNamePrefs = getSharedPreferences("Student", Context.MODE_PRIVATE);
+        studentNameEdit = studentNamePrefs.edit();
+
+        try {
+            keyStore.getInstance("AndroidKeyStore");
+//            keyStore.load(null);
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+
+        }
+
+        //  createNewKeys();
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                login();
+
             }
         });
-    }
+    }*/
 
+    }
 }
