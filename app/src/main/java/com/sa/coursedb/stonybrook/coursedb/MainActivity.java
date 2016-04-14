36e976d9bc0b2bcc70abd9e35f2d459310b23f4e
Sheryar Shah <sheryar.shah53@yahoo.com>
2016-04-14 01:49:08 -0400

    Added Login and Course Activity

--- app/src/main/java/com/sa/coursedb/stonybrook/coursedb/MainActivity.java
+++ app/src/main/java/com/sa/coursedb/stonybrook/coursedb/MainActivity.java
@@ -1,52 +1,37 @@
 package com.sa.coursedb.stonybrook.coursedb;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    public static final String TAG = "MainActivity";
+
+    private SharedPreferences loginPreferences;
+    private Boolean saveLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+        loginPreferences = getSharedPreferences("loginPrefs", MODE_PRIVATE);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        saveLogin = loginPreferences.getBoolean("saveLogin", false);
+        Log.d(TAG, "login value: " + saveLogin);
+        if(saveLogin){
+            Intent intent = new Intent(this, CoursesActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        else{
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
         }
 
-        return super.onOptionsItemSelected(item);
     }
 }
