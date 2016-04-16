8974a45f593299cda852feb39cfbe2e18fe2370e
Sheryar Shah <sheryar.shah53@yahoo.com>
2016-04-16 14:13:26 -0400

    Added Questions Activity, updated Course Activity and xmls for courses and questions activity

--- app/src/main/java/com/sa/coursedb/stonybrook/coursedb/CoursesActivity.java
+++ app/src/main/java/com/sa/coursedb/stonybrook/coursedb/CoursesActivity.java
@@ -1,20 +1,185 @@
 package com.sa.coursedb.stonybrook.coursedb;
 
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class CoursesActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+
+public class CoursesActivity extends Activity {
+
+    public static final String TAG = "CoursesActivity";
+
+    TextView studentName;
+
+    CourseManagment service;
+    ImageView image;
+
+    private SharedPreferences userNamePrefs;
+
+    SharedPreferences courseQuestionPrefs;
+    SharedPreferences.Editor courseQuestionEdit;
+
+    SharedPreferences studentNamePrefs;
+
+    private ArrayList<String> myCourses;
+
+
+    String loggedStudent = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course);
+        image = (ImageView) findViewById(R.id.sbuLogo);
+
+        service = new CourseManagment();
 
 
+        courseQuestionPrefs = getSharedPreferences("CourseQuestion", Context.MODE_PRIVATE);
+        courseQuestionEdit = courseQuestionPrefs.edit();
+
+        userNamePrefs = getSharedPreferences("userNamePrefs", Context.MODE_PRIVATE);
+
+        studentNamePrefs = getSharedPreferences("StudentPrefs", Context.MODE_PRIVATE);
+
+        loggedStudent = studentNamePrefs.getString("studentName", null);
+        Log.d(TAG, "Student name is: " + loggedStudent);
+
+        String netID = userNamePrefs.getString("username", null);
+        Log.d(TAG, "username is: " + netID);
+
+        myCourses = new ArrayList<>();
+
+        CourseAsync courseAsyncTask = new CourseAsync();
+        courseAsyncTask.execute(netID);
+
+    }
+
+    private class CourseAsync extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String netid = params[0];
+
+            try {
+                // open a connection to the site
+                URL url = new URL("http://130.245.191.166:8080/getUserCourses1.php");
+                URLConnection con = url.openConnection();
+                // activate the output
+                con.setDoOutput(true);
+                PrintStream ps = new PrintStream(con.getOutputStream());
+
+                ps.print("userIDKey=" +netid);
+
+                // we have to get the input stream in order to actually send the request
+                con.getInputStream();
+
+                // Get the response
+                BufferedReader rd = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String line;
+                while ((line = rd.readLine()) != null) {
+                    System.out.println(line);
+                    myCourses.add(line);
+
+                }
+                myCourses.remove(0);
+
+                // close the print stream
+                ps.close();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            service.storeCourses(myCourses);
+
+            addCoursesToView();
+
+        }
+
+
+    }
+
+    private void addCoursesToView(){
+
+        studentName = (TextView) findViewById(R.id.studentNameID);
+
+        LinearLayout rl = (LinearLayout) findViewById(R.id.LinearLayout);
+
+        studentName.append(loggedStudent);
+
+        //get the list of courses from server and start adding it the fragment
+        Log.d(TAG, "COURSE SIZE = " + service.getCoursesList().size());
+        for (int i = 0; i < service.getCoursesList().size(); i++) {
+            Button b = new Button(this);
+            b.setBackgroundResource(R.drawable.blackboard);
+            //b.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.arrow_right, 0);
+            b.setText(service.getCoursesList().get(i).getCourseTitle());
+            b.setTextColor(Color.rgb((int) (Math.random() * 255), (int) (Math.random() * 255), (int) (Math.random() * 255)));
+
+            b.setLayoutParams(new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            b.setId(i + 1);
+            final int index = i;
+            // Set click listener for button
+            b.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Log.i(TAG, "index :" + index);
+                    Intent i = new Intent(getApplication(), QuestionsActivity.class);
+                    Bundle bundle = new Bundle();
+                    //  i.putExtra("COURSES", courseList.get(index).toString());
+                    bundle.putParcelable("CourseObject", service.getCoursesList().get(index));
+
+                    Log.d(TAG, "Course clicked on:" +service.getCoursesList().get(index).getCourseTitle());
+
+                    courseQuestionEdit.putString("courseQuest", service.getCoursesList().get(index).getCourseTitle());
+                    courseQuestionEdit.commit();
+                    i.putExtras(bundle);
+                    startActivity(i);
+
+
+                }
+            });
+            rl.addView(b);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(CoursesActivity.this, LoginActivity.class));
+        finish();
     }
 
 }
--- app/src/main/java/com/sa/coursedb/stonybrook/coursedb/LoginActivity.java
+++ app/src/main/java/com/sa/coursedb/stonybrook/coursedb/LoginActivity.java
@@ -1,32 +1,48 @@
 package com.sa.coursedb.stonybrook.coursedb;
 
-import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.security.KeyPairGeneratorSpec;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.math.BigInteger;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.util.ArrayList;
 
+import javax.security.auth.x500.X500Principal;
+
 public class LoginActivity extends AppCompatActivity {
 
     public static final String TAG = "LoginActivity";
-    private static final int REQUEST_SIGNUP = 0;
 
-    KeyStore keyStore;
+    private KeyStore keyStore;
 
     private Button login;
     private EditText user, pass;
     private CheckBox loginCheckBox;
-    private DataOutputStream cgiInput;
     private String wrongCredentials = "";
 
     protected ProgressDialog progressDialog;
@@ -49,18 +65,18 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-  /*      login = (Button) findViewById(R.id.login);
+        login = (Button) findViewById(R.id.login);
         user = (EditText) findViewById(R.id.netid);
         pass = (EditText) findViewById(R.id.password);
-        loginCheckBox = (CheckBox)findViewById(R.id.checkBox);
+        loginCheckBox = (CheckBox) findViewById(R.id.checkBox);
 
         loginPreferences = getSharedPreferences("loginPrefs", MODE_PRIVATE);
         loginPrefsEditor = loginPreferences.edit();
 
-        coursePrefs = getSharedPreferences("Course", Context.MODE_PRIVATE);
-        courseEdit = coursePrefs.edit();
+        userNamePrefs = getSharedPreferences("userNamePrefs", Context.MODE_PRIVATE);
+        userNameEdit = userNamePrefs.edit();
 
-        studentNamePrefs = getSharedPreferences("Student", Context.MODE_PRIVATE);
+        studentNamePrefs = getSharedPreferences("StudentPrefs", Context.MODE_PRIVATE);
         studentNameEdit = studentNamePrefs.edit();
 
         try {
@@ -73,6 +89,81 @@ public class LoginActivity extends AppCompatActivity {
 
         //  createNewKeys();
 
+        login.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                login();
+
+            }
+        });
+    }
+
+    private void createNewKeys() {
+        String alias = "SBCourseDiscussionAPP";
+        try {
+            // Create new key if needed
+            if (!keyStore.containsAlias(alias)) {
+                //  Calendar start = Calendar.getInstance();
+                //  Calendar end = Calendar.getInstance();
+                // end.add(Calendar.YEAR, 1);
+                KeyPairGeneratorSpec spec = new KeyPairGeneratorSpec.Builder(this)
+                        .setAlias(alias)
+                        .setSubject(new X500Principal("CN=Course Discussion, O=Stony Brook"))
+                        .setSerialNumber(BigInteger.ONE)
+                                //     .setStartDate(start.getTime())
+                                //     .setEndDate(end.getTime())
+                        .build();
+                KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA", "AndroidKeyStore");
+                generator.initialize(spec);
+
+                KeyPair keyPair = generator.generateKeyPair();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Exception " + e.getMessage() + " occured", Toast.LENGTH_LONG).show();
+            Log.e(TAG, Log.getStackTraceString(e));
+        }
+    }
+
+    private void login() {
+
+        String netID = user.getText().toString();
+        String password = pass.getText().toString();
+        if (netID.matches("^\\s*$") || password.equalsIgnoreCase("")) {
+            Toast.makeText(getBaseContext(), "Please input all the fields", Toast.LENGTH_LONG).show();
+            return;
+        }
+        Log.d(TAG, "NetID is = " + netID);
+        if (!validateLogin(netID, password)) {
+            onLoginFailed();
+            return;
+        }
+
+        login.setEnabled(false);
+
+    }
+
+    @Override
+    public void onRestart() {
+        super.onRestart();
+        //When BACK BUTTON is pressed, the activity on the stack is restarted
+        Log.d(TAG, "Activity Restarted");
+        Log.d(TAG, "Value of wrong credentals = " + wrongCredentials);
+        login = (Button) findViewById(R.id.login);
+        user = (EditText) findViewById(R.id.netid);
+        pass = (EditText) findViewById(R.id.password);
+
+        try {
+            keyStore.getInstance("AndroidKeyStore");
+//            keyStore.load(null);
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        }
+
+        userNameEdit.clear();
+        userNameEdit.commit();
+
+        //  createNewKeys();
 
         login.setOnClickListener(new View.OnClickListener() {
 
@@ -82,7 +173,262 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
-    }*/
+    }
+
+    public boolean validateLogin(String netID, String password) {
+        boolean valid = true;
+
+        progressDialog = new ProgressDialog(LoginActivity.this);
+        login.setEnabled(false);
+
+        LoginAsync loginAsyncTask = new LoginAsync();
+        loginAsyncTask.execute(netID, password);
+
+        return valid;
+    }
+
+    private class LoginAsync extends AsyncTask<String, Void, String> {
+
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog.setIndeterminate(true);
+            progressDialog.setMessage("Authenticating...");
+            progressDialog.setCanceledOnTouchOutside(false);
+            if (!progressDialog.isShowing())
+                progressDialog.show();
+
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            String netID, password;
+
+            netID = strings[0];
+            password = strings[1];
+
+            try {
+                Log.d(TAG, "Constructing Data");
+                // Construct data
+                StringBuilder dataBuilder = new StringBuilder();
+                dataBuilder.append(URLEncoder.encode("userName=", "UTF-8")).append('=').append(URLEncoder.encode("password", "UTF-8"));
+
+                // Send data
+                Log.d(TAG, "Sending Data");
+               // URL url = new URL("http://10.0.3.2:8080/getCourses");
+
+                URL url = new URL("http://192.168.141.112:8080/getCourses");
+                //home
+                // URL url = new URL("http://192.168.1.237:8080/getCourses");
+                //send over secure connection
+                //   HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+                HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
+                urlConn.setDoInput(true);
+                urlConn.setDoOutput(true);
+                urlConn.setUseCaches(false);
+                urlConn.setReadTimeout(30000 /* milliseconds */);
+                urlConn.setConnectTimeout(30000 /* milliseconds */);
+                urlConn.setRequestProperty("Content-Type",
+                        "application/x-www-form-urlencoded");
+                urlConn.setRequestProperty("Connection", "Keep-Alive");
+                urlConn.connect();
+
+                // Send POST output.
+                Log.d(TAG, "Send Post Output");
+                DataOutputStream cgiInput = new DataOutputStream(urlConn.getOutputStream());
+
+                String content = "userName=" + URLEncoder.encode(netID) + "&password=" + URLEncoder.encode(password);
+
+                cgiInput.writeBytes(content);
+                cgiInput.flush();
+                cgiInput.close();
+
+                // Get the response
+                Log.d(TAG, "Get the response");
+                BufferedReader cgiOutput = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+                String line;
+                int counter = 0;
+                while ((line = cgiOutput.readLine()) != null) {
+                    if (line.equalsIgnoreCase("Wrong Credentials")) {
+                        wrongCredentials = line;
+                        break;
+                    } else {
+                        if (counter == 0) {
+                            studentName = line;
+                        }
+                        counter++;
+                        courses.add(line);
+                    }
+                }
+
+                courses.remove(0);
+
+                for (int i = 0; i < courses.size(); i++) {
+                    Log.d(TAG, i + courses.get(i));
+                }
+
+                cgiOutput.close();
+                urlConn.disconnect();
+
+            } catch (Exception e) {
+                Log.e(TAG, "Connection Error: " + e.toString());
+                serverError = true;
+            }
+
+            return null;
+
+        }
+
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (progressDialog.isShowing()) {
+                progressDialog.dismiss();
+            }
+            Log.d(TAG, "Line is = " + wrongCredentials);
+            if (wrongCredentials.contains("Wrong Credentials")) {
+                onLoginFailed();
+            } else if (serverError) {
+                onServerError();
+            } else {
+
+                onLoginSuccess();
+            }
+
+        }
+    }
+
+    public void onLoginSuccess() {
+
+        ServerAsync serverAsyncTask = new ServerAsync();
+        serverAsyncTask.execute();
 
     }
+
+
+    public void nextActivity() {
+
+        login.setEnabled(true);
+        if (loginCheckBox.isChecked()) {
+            loginPrefsEditor.putBoolean("saveLogin", true);
+            loginPrefsEditor.commit();
+        } else {
+            loginPrefsEditor.clear();
+            loginPrefsEditor.commit();
+        }
+
+        Log.d(TAG, "Login in successful");
+
+        userNameEdit.putString("username", user.getText().toString());
+        userNameEdit.commit();
+
+        Log.d(TAG, "Student name is: " + studentName);
+
+        studentNameEdit.putString("studentName", studentName);
+        studentNameEdit.commit();
+
+
+        Intent i = new Intent(getApplication(), CoursesActivity.class);
+        startActivity(i);
+
+    }
+
+    private class ServerAsync extends AsyncTask<String, Void, String> {
+
+        String netID = user.getText().toString();
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+                // open a connection to the site
+                URL url = new URL("http://130.245.191.166:8080/users1.php");
+                URLConnection con = url.openConnection();
+                // activate the output
+                con.setDoOutput(true);
+                PrintStream ps = new PrintStream(con.getOutputStream());
+
+                ps.print("userIDKey=" + netID);
+
+                // we have to get the input stream in order to actually send the request
+                con.getInputStream();
+
+                // close the print stream
+                ps.close();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            for (int i = 0; i < courses.size(); i++) {
+                try {
+
+                    // open a connection to the site
+                    URL url = new URL("http://130.245.191.166:8080/userCourses1.php");
+                    URLConnection con = url.openConnection();
+                    // activate the output
+                    con.setDoOutput(true);
+                    PrintStream ps = new PrintStream(con.getOutputStream());
+
+                    ps.print("userIDKey=" + netID);
+                    ps.print("&courseKey=" + courses.get(i).toString());
+
+                    // we have to get the input stream in order to actually send the request
+                    con.getInputStream();
+
+                    // close the print stream
+                    ps.close();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            nextActivity();
+
+        }
+    }
+
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Incorrect netID or Password", Toast.LENGTH_LONG).show();
+        wrongCredentials = "";
+        user.setText("");
+        pass.setText("");
+        login.setEnabled(true);
+        loginCheckBox.setChecked(false);
+    }
+
+    public void onServerError() {
+        Toast.makeText(getBaseContext(), "Server Connection Time Out Error", Toast.LENGTH_LONG).show();
+        user.setText("");
+        pass.setText("");
+        login.setEnabled(true);
+        loginCheckBox.setChecked(false);
+        serverError = false;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Log.d(TAG, "onBackPressed Called");
+        login.setEnabled(true);
+        this.finish();
+        System.exit(0);
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+    }
+
+
 }
